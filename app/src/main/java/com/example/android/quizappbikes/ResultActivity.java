3b3a897dea9e15ddd732080ce39d7c8c3366e2a3
Sabina Ioana <sabinaleitoiu91@gmail.com>
2017-06-28 17:24:06 +0100

    QuizAppBikes

--- app/src/main/java/com/example/android/quizappbikes/ResultActivity.java
+++ app/src/main/java/com/example/android/quizappbikes/ResultActivity.java
@@ -25,27 +25,19 @@ public class ResultActivity extends AppCompatActivity {
             finalView.setText("CRUISER, you are rater laid-back. You embrace carefree days and thoroughly enjoy the sweet, simple things in life.");
             finalImage.setImageResource(R.drawable.cruiser_bike);
             Toast.makeText(getApplicationContext(), R.string.summary1, Toast.LENGTH_SHORT).show();
-        }
-
-        if ((score >= 11) && (score < 16)) {
+        } else  if ((score >= 11) && (score < 16)) {
             finalView.setText("HYBRID BIKE. You are the person who is ready for anything and everything that your ride route (and life) may throw your way.");
             finalImage.setImageResource(R.drawable.hybrid_bike);
             Toast.makeText(getApplicationContext(), R.string.summary2, Toast.LENGTH_SHORT).show();
-        }
-
-        if ((score >= 6) && (score < 24)) {
+        } else if ((score >= 6) && (score < 24)) {
             finalView.setText("ROAD BIKE. Energetic and dedicated, you are both loyal leader and team player. You value endurance and doing what it takes to get a job done-and done right.");
             finalImage.setImageResource(R.drawable.road_bike);
             Toast.makeText(getApplicationContext(), R.string.summary3, Toast.LENGTH_SHORT).show();
-        }
-
-        if ((score >= 24) && (score < 32)) {
+        } else if ((score >= 24) && (score < 32)) {
             finalView.setText("BMX BIKE. You are an adrenaline-junkie. You love the spotlight and will often do whatever it takes to maintain your audience, even if it involves some risk. The bigger the thrills and spills, the better!");
             finalImage.setImageResource(R.drawable.bmx_bike);
             Toast.makeText(getApplicationContext(), R.string.summary4, Toast.LENGTH_SHORT).show();
-        }
-
-        if (score >= 32) {
+        } else {
             finalView.setText("TREADLE BIKE. You are clearly a trend-setter. You are eccentric and love finding hobbies that set you apart from the crowd.");
             finalImage.setImageResource(R.drawable.treadle_bike);
             Toast.makeText(getApplicationContext(), R.string.summary5, Toast.LENGTH_SHORT).show();
